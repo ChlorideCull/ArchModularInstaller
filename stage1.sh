e432e95b442b37c01846edde244162c2a0c33160
Chloride Cull <chloride@devurandom.net>
2015-05-15 15:57:29 +0200

    initial monolitic version

--- stage1.sh
+++ stage1.sh
@@ -38,6 +38,7 @@ find /usr/share/kbd/keymaps/ -type f -printf '%f ' > /var/tmp/inst_dfl
 echo "---- Step 1 ---- Select Keymap ----"
 doFullList "/var/tmp/inst_dfl"
 loadkeys "$doFullList_ret"
+keymap="$doFullList_ret"
 
 
 keeplooping="Yes"
@@ -45,15 +46,16 @@ while [[ "$keeplooping" == "Yes" ]]; do
 	echo
 	echo "---- Step 2 ----   Partition   ----"
 	echo "Step 2 and 3 repeats until you have finished all partitions."
+	echo "*MAKE SURE TO SET THE BOOTABLE FLAG WITH a ON THE BOOT/ROOT PARTITION*"
 	echo
 	echo "Skip" > /var/tmp/inst_dfl
-	tmpvar=""
 	for disknod in "sd" "vd" "hd"; do
-		tmpvar="$(find /dev -name "${disknod}*[^0-9]") $tmpvar"
+		find /dev -name "${disknod}*[^0-9]" >> /var/tmp/inst_dfl
 	done
-	cp /var/tmp/inst_dfl /var/tmp/inst_dfl_item
-	> /var/tmp/inst_dfl <<<"$tmpvar"
+	mv /var/tmp/inst_dfl /var/tmp/inst_dfl_item
+	tr '\n' ' ' < /var/tmp/inst_dfl_item > /var/tmp/inst_dfl
 	doFullList "/var/tmp/inst_dfl"
+	disks=$(cat /var/tmp/inst_dfl)
 	rm /var/tmp/inst_dfl /var/tmp/inst_dfl_item
 	if [[ "$doFullList_ret" != "Skip" ]]; then
 		echo "------------"
@@ -63,11 +65,12 @@ while [[ "$keeplooping" == "Yes" ]]; do
 
 	echo
 	echo "---- Step 3 ----     Format    ----"
-	tmpvar=""
+	echo -n "" > /var/tmp/inst_dfl
 	for disknod in "sd" "vd" "hd"; do
-		 tmpvar="$(find /dev -name "${disknod}*[0-9]") $tmpvar"
+		 find /dev -name "${disknod}*[0-9]"  >> /var/tmp/inst_dfl
 	done
-	> /var/tmp/inst_dfl <<<"$tmpvar"
+	mv /var/tmp/inst_dfl /var/tmp/inst_dfl_item
+	tr '\n' ' ' < /var/tmp/inst_dfl_item > /var/tmp/inst_dfl
 	doFullList "/var/tmp/inst_dfl"
 	disknod="$doFullList_ret"
 	echo "LVM Swap $(find /usr/bin/ -name 'mkfs.*' -printf '%f ')" > /var/tmp/inst_dfl
@@ -120,6 +123,10 @@ done
 
 echo
 echo "---- Step 4 ----   Configure   ----"
+
+echo -n "Hostname? "
+read hstnme
+
 echo -n "Install development tools (y/n)? "
 read devtools
 if [[ "$devtools" == "y" ]]; then
@@ -128,24 +135,27 @@ else
 	devtools=""
 fi
 
+echo
 echo "Install bootloader on:"
 echo "(You most likely want the disk that /boot resides on, or it's LVM physical volume)"
-bootloader_install=""
-for potentialdevice in / /boot; do
-	bootloader_install="/dev/$(basename "$(dirname "$(readlink "/sys/dev/block/$(stat "$(findmnt -o SOURCE -e -n "$potentialdevice")" | awk 'NR==3{print()}' | tr ',' ':')")")")" $bootloader_install"
-done
-> /var/tmp/possibledevs <<<"$bootloader_install"
+echo "$disks" | sed 's/Skip//' > /var/tmp/possibledevs
 doFullList "/var/tmp/possibledevs"
 bootloader_install="$doFullList_ret"
 
-#TODO: Ask for timezone
+echo
+echo "Select time zone"
+curdir="$PWD"
+cd /usr/share/zoneinfo
+find ./ -type f -printf '%p ' > /var/tmp/inst_dfl
+cd "$curdir"
+doFullList "/var/tmp/inst_dfl"
 tz="$doFullList_ret"
 
-#TODO: Ask for locale, grab from locale.gen
+echo
+echo "Select locale to use"
+grep -E '(.+) UTF-8' /etc/locale.gen | sed 's/# *//g' | cut -d' ' -f1 | tr '\n' ' ' > /var/tmp/inst_dfl
+doFullList "/var/tmp/inst_dfl"
 locale="$doFullList_ret"
-
-#TODO: Ask for hostname
-hstnme="$doFullList_ret"
 
 pacstrap /mnt/system/ base$devtools
 genfstab /mnt/system/ > /mnt/system/etc/fstab
@@ -159,13 +169,15 @@ echo "bootloader_install=\"$bootloader_install\"" >> /mnt/system/root/stage2.sh
 echo "tz=\"$tz\"" >> /mnt/system/root/stage2.sh
 echo "locale=\"$locale\"" >> /mnt/system/root/stage2.sh
 echo "hstnme=\"$hstnme\"" >> /mnt/system/root/stage2.sh
+echo "keymap=\"$keymap\"" >> /mnt/system/root/stage2.sh
 >> /mnt/system/root/stage2.sh << EOF
 #CONFIG END
 #--- The script below is automatically generated from stage2.sh ---
 STAGE2MARKER
+
 EOF
 chmod +x /mnt/system/root/stage2.sh
 arch-chroot /mnt/system/ /root/stage2.sh
-echo "All done! Hit any key to reboot the system."
+echo "All done! Hit enter to reboot the system."
 read whyareyoulookingatthis
 shutdown -r now
