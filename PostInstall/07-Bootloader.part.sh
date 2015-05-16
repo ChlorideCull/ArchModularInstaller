4b346c4a35d66a65db5b2af7e860fe16ee57b347
Chloride Cull <chloride@devurandom.net>
2015-05-16 12:49:58 +0200

    try to get lvm install working

--- PostInstall/07-Bootloader.part.sh
+++ PostInstall/07-Bootloader.part.sh
@@ -1,4 +1,3 @@
 pacman -Syu --noconfirm grub os-prober
-
 grub-install "$bootloader_install"
 grub-mkconfig > /boot/grub/grub.cfg
--- PreInstall/02-PartitionFormat.part.sh
+++ PreInstall/02-PartitionFormat.part.sh
@@ -1,42 +1,16 @@
-keeplooping="Yes"
-while [[ "$keeplooping" == "Yes" ]]; do
-	echo
-	echo "---- Step 2 ----   Partition   ----"
-	echo "Step 2 and 3 repeats until you have finished all partitions."
-	echo "*MAKE SURE TO SET THE BOOTABLE FLAG WITH a ON THE BOOT/ROOT PARTITION*"
-	echo
-	echo "Skip" > /tmp/inst_dfl
-	for disknod in "sd" "vd" "hd"; do
-		find /dev -name "${disknod}*[^0-9]" >> /tmp/inst_dfl
-	done
-	mv /tmp/inst_dfl /tmp/inst_dfl_item
-	tr '\n' ' ' < /tmp/inst_dfl_item > /tmp/inst_dfl
-	doFullList "/tmp/inst_dfl"
-	disks=$(cat /tmp/inst_dfl)
-	rm /tmp/inst_dfl /tmp/inst_dfl_item
-	if [[ "$doFullList_ret" != "Skip" ]]; then
-		echo "------------"
-		fdisk "$doFullList_ret"
-		echo "------------"
-	fi
+lvmused=1
 
-	echo
-	echo "---- Step 3 ----     Format    ----"
-	echo -n "" > /tmp/inst_dfl
-	for disknod in "sd" "vd" "hd"; do
-		 find /dev -name "${disknod}*[0-9]"  >> /tmp/inst_dfl
-	done
-	mv /tmp/inst_dfl /tmp/inst_dfl_item
-	tr '\n' ' ' < /tmp/inst_dfl_item > /tmp/inst_dfl
-	doFullList "/tmp/inst_dfl"
-	disknod="$doFullList_ret"
+function doFormat {
+	disknod="$1"
+	echo "Select file system:"
 	echo "LVM Swap $(find /usr/bin/ -name 'mkfs.*' -printf '%f ')" > /tmp/inst_dfl
 	doFullList "/tmp/inst_dfl"
 	if [[ "$doFullList_ret" == "LVM" ]]; then
+		lvmused=0
 		echo "------------"
 		pvcreate "$disknod"
 		echo "------------"
-		echo -n "Create new volume group? (y/<existing group name) "
+		echo -n "Create new volume group or use existing? (y/<existing group name) "
 		read mkvg
 		if [[ "$mkvg" == "y" ]]; then
 			echo -n "Volume Group Name? "
@@ -44,13 +18,26 @@ while [[ "$keeplooping" == "Yes" ]]; do
 			echo "------------"
 			vgcreate "$vgname" "$disknod"
 			echo "------------"
-			echo -n "Logical Volume Name? "
-			read lvname
-			echo -n "Size (*G)? "
-			read lvsize
-			echo "------------"
-			lvcreate -L "$lvsize" -n "$lvname" "$vgname"
-			echo "------------"
+			doanotherlv="Yes"
+			while [[ "$doanotherlv" == "Yes" ]]; do
+				echo -n "Logical Volume Name? "
+				read lvname
+				echo -n "Size (*G)? "
+				read lvsize
+				echo "------------"
+				lvcreate -L "$lvsize" -n "$lvname" "$vgname"
+				echo "------------"
+				echo "Format?"
+				select shouldformat in Yes No; do
+					if [[ "$shouldformat" == "Yes" ]]; then
+						doFormat "/dev/mapper/${vgname}-${lvname}"
+					fi
+				done
+				echo "Create another logical volume?"
+				select doanotherlv in Yes No; do
+					break;
+				done
+			done
 		else
 			echo "------------"
 			vgextend "$mkvg" "$disknod"
@@ -70,6 +57,44 @@ while [[ "$keeplooping" == "Yes" ]]; do
 			break;
 		done
 	fi
+}
+
+keeplooping="Yes"
+while [[ "$keeplooping" == "Yes" ]]; do
+	echo
+	echo "---- Step 2 ----   Partition   ----"
+	echo "Step 2 and 3 repeats until you have finished all partitions."
+	echo "* MAKE SURE TO SET THE BOOTABLE FLAG WITH a ON THE BOOT/ROOT PARTITION *"
+	echo "* IF YOU WILL USE LVM, SET PARTITION TYPE TO 8e                        *"
+	echo
+	echo "Skip" > /tmp/inst_dfl
+	for disknod in "sd" "vd" "hd"; do
+		find /dev -name "${disknod}*[^0-9]" >> /tmp/inst_dfl
+	done
+	mv /tmp/inst_dfl /tmp/inst_dfl_item
+	tr '\n' ' ' < /tmp/inst_dfl_item > /tmp/inst_dfl
+	doFullList "/tmp/inst_dfl"
+	disks=$(cat /tmp/inst_dfl)
+	rm /tmp/inst_dfl /tmp/inst_dfl_item
+	if [[ "$doFullList_ret" != "Skip" ]]; then
+		echo "------------"
+		fdisk "$doFullList_ret"
+		echo "------------"
+	fi
+
+	echo
+	echo "---- Step 3 ----     Format    ----"
+	echo "Select partition:"
+	echo -n "" > /tmp/inst_dfl
+	for disknod in "sd" "vd" "hd"; do
+		 find /dev -name "${disknod}*[0-9]"  >> /tmp/inst_dfl
+	done
+	mv /tmp/inst_dfl /tmp/inst_dfl_item
+	tr '\n' ' ' < /tmp/inst_dfl_item > /tmp/inst_dfl
+	doFullList "/tmp/inst_dfl"
+	
+	doFormat "$doFullList_ret"
+	
 	echo
 	echo "Do another partition?"
 	select keeplooping in Yes No; do
